b88fdc871860cbf3c7d06315b0841277fd0b5f50
Nestor <nestoramichetti@neuronic.com.ar>
2021-02-09 19:51:47 -0300

    Agregado mapa y marcador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../app/service/weather.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { debounceTime } from 'rxjs/operators';
+import { environment } from '../environments/environment';
+import * as mapboxgl from 'mapbox-gl'; 
 
 @Component({
   selector: 'app-root',
@@ -19,12 +20,17 @@ export class AppComponent implements OnInit{
   temperatureMin;
   feelsLike;
   toCelsius:number = 273.15;
+  lat;
+  lon;
+  map:mapboxgl.Map;
 
   constructor(private wheaterService:WeatherService, private formBuilder:FormBuilder){
   }
 
   ngOnInit(){
     this.buildForm();
+    this.mapBox('-74.5', '40');
+    this.marker('-74.5', '40');
   }
 
   private buildForm() {
@@ -38,7 +44,6 @@ export class AppComponent implements OnInit{
     event.preventDefault();
     if(this.form.valid){
       const value = this.form.value;
-      console.log(value.city + ' y ' + value.country)
       this.getWeather(value.city,value.country)
     }else{
       this.form.markAllAsTouched();
@@ -63,13 +68,34 @@ export class AppComponent implements OnInit{
         this.feelsLike      = this.weather.main.feels_like   - this.toCelsius;
         this.pressure       = this.weather.main.pressure;
         this.description    = this.weather.weather[0].description;
-        this.humidity       = this.weather.main.humidity
-        console.log(this.weather)
+        this.humidity       = this.weather.main.humidity;
+        this.lat            = this.weather.coord.lat;
+        this.lon            = this.weather.coord.lon;
+        this.mapBox(this.lon,this.lat);
+        this.marker(this.lon,this.lat);
       },
         err => {
           console.log(err)
         }
     )
+  }
+
+  mapBox(lon,lat){
+  mapboxgl.accessToken = 'pk.eyJ1Ijoic3lkYmFycmV0dCIsImEiOiJja2t5M2M3Y3IxdWVvMnZwaDA4MWhzMjlqIn0.KjT_FAuCADC9UH0ZyFwzlQ';
+    this.map = new mapboxgl.Map({
+    container: 'map-mapbox',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [Number(lon),Number(lat)], // lon and lat
+    zoom: 11
+    });
+  }
+
+  marker(lon,lat){
+  const marker = new mapboxgl.Marker({
+  color: "red",
+  draggable: false
+  }).setLngLat([Number(lon), Number(lat)])
+  .addTo(this.map);
   }
 
 }
