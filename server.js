c5a01a302e8f71684cd04940fbfdd7efc931ff0b
Nestor <nestoramichetti@neuronic.com.ar>
2021-02-10 12:02:00 -0300

    Cambiando server.js

--- server.js
+++ server.js
@@ -1,35 +1,17 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
 
 const app = express();
 
-// add this code
-const whitelist = ['http://localhost:3000']; // list of allow domain
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin) {
-            return callback(null, true);
-        }
-
-        if (whitelist.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}
-
-// end 
-app.use(cors(corsOptions));
-
+// Serve only the static files form the angularapp directory
 app.use(express.static(__dirname + '/angularapp'));
 
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/angularapp/index.html'));
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/angularapp/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080); 
+
 
