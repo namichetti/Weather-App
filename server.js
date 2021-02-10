4b40c5005e7f2777b5d93848db5575b8183bac29
Nestor <nestoramichetti@neuronic.com.ar>
2021-02-10 10:39:01 -0300

    Agregado CORS a server.js

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
-//Install express server
 const express = require('express');
 const path = require('path');
- 
+const cors = require('cors');
+
 const app = express();
- 
-// Serve only the static files form the angularapp directory
+
+// add this code
+const whitelist = ['http://localhost:4200']; // list of allow domain
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
+
+// end 
+app.use(cors(corsOptions));
+
 app.use(express.static(__dirname + '/angularapp'));
- 
-app.get('/*', function(req,res) {
- 
-res.sendFile(path.join(__dirname+'/angularapp/index.html'));
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/angularapp/index.html'));
 });
- 
-// Start the app by listening on the default Heroku port
+
 app.listen(process.env.PORT || 8080);
+
